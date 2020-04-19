76c5da7fb95a383fc41c0191e814db501470e28f
rivzadrian <mr.adrian40@gmail.com>
2020-04-19 12:43:00 +0700

    Update insyallah klasifikasi sudah rapih

--- Script fix.R
+++ Script fix.R
@@ -12,7 +12,8 @@ library(randomForest)
 library(caret)
 library(reshape2)
 library(rpart.plot)
-
+library(nnet)
+library(e1071)
 bank <- read.csv("Bank Marketing.csv")
 
 head(bank)
@@ -83,7 +84,7 @@ ggcorrplot(corr, hc.order = TRUE,
 
 plot(x=bank$contact, y=bank$y, xlab = "contact", ylab="Income >50K Probability", ylim=c(0,1))
 
-#Membagi data train dan test
+########################################## Membagi data train dan test################################
 set.seed(1234)
 
 sample_size <- floor(0.7*nrow(bank))
@@ -104,7 +105,7 @@ evaluation <- function(data,accuracy){
   cat(paste("F-measure:\t", format(f, digits=6), "\n",sep=" "))
 }
 
-#Decision tree Gini
+########################################## Decision tree Gini###############################
 
 dtreemodel_rpart_gini <- rpart(y~., train_data, parms = list(split = "gini"))
 predict_dtreerpart_gini <- predict(dtreemodel_rpart_gini, test_data[,1:16], type="class")
@@ -116,7 +117,7 @@ resultdt <- table(predict_dtreerpart_gini, test_data$y)
 cmdt <- confusionMatrix(predict_dtreerpart_gini, test_data$y)
 evaluation(resultdt, accuracy = cmdt)#Menampilakn precision recall dan f-measure
 
-#Prune tree dengan indikator CP GINI
+########################################## Prune tree dengan indikator CP GINI#######################
 ptree <- prune(dtreemodel_rpart_gini,cp = dtreemodel_rpart_gini$cptable[which.min(dtreemodel_rpart_gini$cptable[,"xerror"]),"CP"])
 rpart.plot(ptree, uniform=TRUE, main="Pruned Classification Gini Tree")
 printcp(ptree)
@@ -130,7 +131,7 @@ plot(perf, avg= "threshold", colorize=T, lwd= 3,
 plot(perf, lty=3, col="grey78", add=T)
 
 
-#Decision tree entrophy
+########################################## Decision tree entrophy##########################
 dtreemodel_rpart_entro <- rpart(y~., train_data, parms = list(split = "information"))
 predict_dtreerpart_entro <- predict(dtreemodel_rpart_entro, test_data[,1:16], type="class")
 confusionMatrix(predict_dtreerpart_entro, test_data$y)
@@ -141,7 +142,7 @@ resultdt <- table(predict_dtreerpart_entro, test_data$y)
 cmdt <- confusionMatrix(predict_dtreerpart_entro, test_data$y)
 evaluation(resultdt, accuracy = cmdt)#Menampilakn precision recall dan f-measure
 
-#Prune tree dengan indikator CP Entrophy
+########################################## Prune tree dengan indikator CP Entrophy###################
 ptree <- prune(dtreemodel_rpart_entro,cp = dtreemodel_rpart_entro$cptable[which.min(dtreemodel_rpart_entro$cptable[,"xerror"]),"CP"])
 rpart.plot(ptree, uniform=TRUE, main="Pruned Classification Entrophy Tree")
 printcp(ptree)
@@ -150,7 +151,7 @@ predict_dtreerpart <- predict(ptree, test_data[,1:16], type="class")
 confusionMatrix(predict_dtreerpart, test_data$y)
 
 
-#Naive bayes
+########################################## Naive bayes tes siginifikansi kolom############################
 #Building a model
 
 library(e1071)
@@ -190,8 +191,8 @@ x = train_data[,-11]
 y = train_data$y
 
 
-#################################naive bayes yg praktikum no laplace###########################
-nbmodel <- naiveBayes(y~., data=train_data)
+########################################## naive bayes no laplace###########################
+nbmodel <- naiveBayes(y~., data=train_data, laplace = 0)
 nbpredict <- predict(nbmodel, test_data[,1:12], type="class")
 
 confusionMatrix(nbpredict,test_data$y)
@@ -208,7 +209,7 @@ confusionMatrix(predict.nb, test_data$y)
 X <- varImp(fit.nb)
 plot(X)
 
-###############################naive bayes yg praktikum laplace#####################
+########################################## naive bayes laplace##############################
 nbmodel <- naiveBayes(y~., data=train_data, laplace = 1)
 nbpredict <- predict(nbmodel, test_data[,1:12], type="class")
 
@@ -226,7 +227,7 @@ confusionMatrix(predict.nb, test_data$y)
 X <- varImp(fit.nb)
 plot(X)
 
-#praprocessing untuk ANN dan kNN
+########################################## praprocessing untuk ANN dan kNN#################################
 bank<- read.csv("Bank Marketing.csv")
 #Merubah data menjadi numerrik
 bank$housing <- as.character(bank$housing)
@@ -247,18 +248,15 @@ bank$month <- factor(bank$month,
                                 "sep", "oct", "nov", "dec"))
 bank$month <- as.numeric(bank$month)
 
-str(bank$contact)
-
 bank$contact <- factor(bank$contact, 
                        levels = c("unknown","cellular","telephone"))
 bank$contact <- as.numeric(bank$contact)
 
-str(bank$education)
 bank$education <- factor(bank$education, 
                          levels = c("unknown","primary","secondary", "tertiary"))
 bank$education <- as.numeric(bank$contact)
 
-str(bank$poutcome)
+
 bank$poutcome <- ifelse(bank$poutcome == "succes",1,0)
 
 
@@ -272,7 +270,7 @@ train_data <- bank[train_index,]
 test_data <- bank[-train_index,]
 
 
-#ann yg lain harus numerik
+########################################## ANN###################################################
 library(nnet)
 library(NeuralNetTools)
 nn1 <- nnet(y~., data = train_data, size = 10, maxit = 300,
@@ -289,13 +287,13 @@ plot.nnet(nn1, alpha.val = 0.5, circle.col = list('lightgray', 'white'), bord.co
 dev.off()
 
 
-#Normalizing data
+########################################## Normalizing data untuk kNN##########################
 normalize <- function(x) {
   return ((x - min(x)) / (max(x) - min(x)))
 }
 head(bank_normal)
 bank_normal <- bank
-bank_normal$Ã¯..age <- normalize(bank_normal$Ã¯..age)
+bank_normal$ï..age <- normalize(bank_normal$ï..age)
 bank_normal$balance <- normalize(bank_normal$balance)
 bank_normal$duration <- normalize(bank_normal$duration)
 
@@ -310,7 +308,7 @@ test_data <- bank[-train_index,]
 #mencari kNN yang optimal
 NROW(train_data) #177 178
 
-#kNN
+########################################## kNN########################################
 
 knnModel212 <- knn3(y~., data = train_data, k = 177)
 knnModel213 <- knn3(y~., data = train_data, k = 178)
@@ -339,15 +337,17 @@ plot(k.opt, type = "b", xlim = c(174, 178), ylim = c(0.886, 0.888))
 
 
 
-######################################## Repeated holdout tree 1 ########################################
+############################################### END Masuk Evaluasi ##################################################
+########################################## Repeated holdout tree Gini ########################################
 # Repeated Holdout tree 1
+#ingat ubah data bank menjadi normal kembali untuk menjalankan ini (baris 16-27)
 library(rminer)
 full_accuracy = 0
 list_accuracy <- list()
 
 for(i in 1:100) {
   H = holdout(bank$y, ratio = 2/3, mode="random", seed = NULL)
-  dtreemodel_rpart <- rpart(y~., bank[H$tr,])
+  dtreemodel_rpart <- rpart(y~., bank[H$tr,], parms = list(split = "gini"))
   predict_dtreerpart <- predict(dtreemodel_rpart, bank[H$ts,1:16], type="class")
   result<- confusionMatrix(predict_dtreerpart, bank[H$ts,]$y)
   accuracy <- result$overall['Accuracy']
@@ -363,14 +363,38 @@ cat("Tree: ",
 x <- c(1:100)
 plot(x, list_accuracy, type="o")
 
-########################################### Repeated Holdout tree 2#############################
+########################################## Repeated holdout tree Entrophy ########################################
+#ingat ubah data bank menjadi normal kembali untuk menjalankan ini (baris 16-27)
 library(rminer)
 full_accuracy = 0
 list_accuracy <- list()
 
 for(i in 1:100) {
   H = holdout(bank$y, ratio = 2/3, mode="random", seed = NULL)
-  dtreemodel_rpart <- rpart(y~., bank[H$tr,])
+  dtreemodel_rpart <- rpart(y~., bank[H$tr,], parms = list(split = "information"))
+  predict_dtreerpart <- predict(dtreemodel_rpart, bank[H$ts,1:16], type="class")
+  result<- confusionMatrix(predict_dtreerpart, bank[H$ts,]$y)
+  accuracy <- result$overall['Accuracy']
+  cat("batch: ",i,
+      "accuracy: ",accuracy,"\n")
+  full_accuracy = full_accuracy + accuracy
+  list_accuracy[[i]] <- accuracy
+}
+
+### Plot
+cat("Tree: ",
+    "Accuracy: ", full_accuracy/100, "\n")
+x <- c(1:100)
+plot(x, list_accuracy, type="o")
+########################################## Repeated Holdout tree Gini prunned#############################
+#ingat ubah data bank menjadi normal kembali untuk menjalankan ini (baris 16-27)
+library(rminer)
+full_accuracy = 0
+list_accuracy <- list()
+
+for(i in 1:100) {
+  H = holdout(bank$y, ratio = 2/3, mode="random", seed = NULL)
+  dtreemodel_rpart <- rpart(y~., bank[H$tr,], parms = list(split = "information"))
   ptree <- prune(dtreemodel_rpart,cp = dtreemodel_rpart$cptable[which.min(dtreemodel_rpart$cptable[,"xerror"]),"CP"])
   predict_dtreerpart <- predict(ptree, bank[H$ts,1:16], type="class")
   result<- confusionMatrix(predict_dtreerpart, bank[H$ts,]$y)
@@ -390,7 +414,8 @@ plot(x, list_accuracy, type="o")
 rataan_akurasi_tree2 <- do.call(sum,list_accuracy)/NROW(list_accuracy)
 rataan_akurasi_tree2
 
-################################################## Cross Validation Tree 1 ####################################
+########################################## Cross Validation Tree Gini ####################################
+#ingat ubah data bank menjadi normal kembali untuk menjalankan ini (baris 16-27)
 ### Membuat 10 fold
 folds <- cut(seq(1,nrow(bank)), breaks=10, label=FALSE)
 full_accuracy_cv = 0
@@ -417,10 +442,65 @@ cat("Tree: ",
 x <- c(1:10)
 plot(x, list_accuracy_cv, type="o")
 
-################################################## END ##################################################
+########################################## Cross Validation Tree Entrophy ####################################
+#ingat ubah data bank menjadi normal kembali untuk menjalankan ini (baris 16-27)
+### Membuat 10 fold
+folds <- cut(seq(1,nrow(bank)), breaks=10, label=FALSE)
+full_accuracy_cv = 0
+list_accuracy_cv <- list()
 
-######################################## Repeated holdout buat Naive Bayes ########################################
+for(i in 1:10) {
+  testIndex <- which(folds == i, arr.ind = TRUE)
+  testData <- bank[testIndex, ]
+  trainData <- bank[-testIndex, ]
+  tree <- rpart(y~., data = trainData, parms = list(split = "information") )
+  treepredict <- predict(tree, testData[,1:16], type="class")
+  result <- confusionMatrix(treepredict, testData$y)
+  accuracy <- result$overall['Accuracy']
+  
+  cat("batch: ",i,
+      "accuracy: ",accuracy,"\n")
+  full_accuracy_cv = full_accuracy_cv + accuracy
+  list_accuracy_cv[[i]] <- accuracy
+}
 
+### Plot
+cat("Tree: ",
+    "Accuracy: ", full_accuracy_cv/10,"\n")
+x <- c(1:10)
+plot(x, list_accuracy_cv, type="o")
+########################################## Cross Validation Tree Gini prunned ####################################
+#ingat ubah data bank menjadi normal kembali untuk menjalankan ini (baris 16-27)
+### Membuat 10 fold
+folds <- cut(seq(1,nrow(bank)), breaks=10, label=FALSE)
+full_accuracy_cv = 0
+list_accuracy_cv <- list()
+
+for(i in 1:10) {
+  testIndex <- which(folds == i, arr.ind = TRUE)
+  testData <- bank[testIndex, ]
+  trainData <- bank[-testIndex, ]
+  tree <- rpart(y~., data = trainData)
+  ptree <- prune(tree,cp = tree$cptable[which.min(tree$cptable[,"xerror"]),"CP"])
+  treepredict <- predict(ptree, testData[,1:16], type="class")
+  result <- confusionMatrix(treepredict, testData$y)
+  accuracy <- result$overall['Accuracy']
+  
+  cat("batch: ",i,
+      "accuracy: ",accuracy,"\n")
+  full_accuracy_cv = full_accuracy_cv + accuracy
+  list_accuracy_cv[[i]] <- accuracy
+}
+
+### Plot
+cat("Tree: ",
+    "Accuracy: ", full_accuracy_cv/10,"\n")
+x <- c(1:10)
+plot(x, list_accuracy_cv, type="o")
+
+
+########################################## Repeated holdout buat Naive Bayes No Laplace ########################################
+#ingat ubah data bank menjadi normal kembali untuk menjalankan ini
 # Repeated Holdout naive bayes
 library(rminer)
 full_accuracy = 0
@@ -444,7 +524,33 @@ cat("Tree: ",
 x <- c(1:100)
 plot(x, list_accuracy, type="o")
 
-################################################## Cross Validation Naive bayes ####################################
+########################################## Repeated holdout Naive Bayes Laplace ########################################
+#ingat ubah data bank menjadi normal kembali untuk menjalankan ini
+# Repeated Holdout naive bayes
+library(rminer)
+full_accuracy = 0
+list_accuracy <- list()
+
+for(i in 1:100) {
+  H = holdout(bank$y, ratio = 2/3, mode="random", seed = NULL)
+  nbmodel <- naiveBayes(y~., data=bank_drop[H$tr,], laplace = 1)
+  nbpredict <- predict(nbmodel, bank_drop[H$ts,1:12], type="class")
+  result<- confusionMatrix(nbpredict, bank_drop[H$ts,]$y)
+  accuracy <- result$overall['Accuracy']
+  cat("batch: ",i,
+      "accuracy: ",accuracy,"\n")
+  full_accuracy = full_accuracy + accuracy
+  list_accuracy[[i]] <- accuracy
+}
+
+### Plot
+cat("Tree: ",
+    "Accuracy: ", full_accuracy/100, "\n")
+x <- c(1:100)
+plot(x, list_accuracy, type="o")
+
+########################################## Cross Validation Naive bayes No Laplace ####################################
+#ingat ubah data bank menjadi normal kembali untuk menjalankan ini
 ### Membuat 10 fold
 folds <- cut(seq(1,nrow(bank_drop)), breaks=10, label=FALSE)
 full_accuracy_cv = 0
@@ -471,8 +577,36 @@ cat("Tree: ",
 x <- c(1:10)
 plot(x, list_accuracy_cv, type="o")
 
-######################################## Repeated holdout buat KNN ########################################
+########################################## Cross Validation Naive bayes Laplace ####################################
+#ingat ubah data bank menjadi normal kembali untuk menjalankan ini
+### Membuat 10 fold
+folds <- cut(seq(1,nrow(bank_drop)), breaks=10, label=FALSE)
+full_accuracy_cv = 0
+list_accuracy_cv <- list()
 
+for(i in 1:10) {
+  testIndex <- which(folds == i, arr.ind = TRUE)
+  testData <- bank_drop[testIndex, ]
+  trainData <- bank_drop[-testIndex, ]
+  nbmodel <- naiveBayes(y~., data=trainData, laplace = 1)
+  nbpredict <- predict(nbmodel, testData, type="class")
+  result <- confusionMatrix(nbpredict, testData$y)
+  accuracy <- result$overall['Accuracy']
+  
+  cat("batch: ",i,
+      "accuracy: ",accuracy,"\n")
+  full_accuracy_cv = full_accuracy_cv + accuracy
+  list_accuracy_cv[[i]] <- accuracy
+}
+
+### Plot
+cat("Tree: ",
+    "Accuracy: ", full_accuracy_cv/10,"\n")
+x <- c(1:10)
+plot(x, list_accuracy_cv, type="o")
+
+########################################## Repeated holdout buat kNN k = 177########################################
+#ingat ubah data bank menjadi NUMERIK kembali untuk menjalankan ini
 # Repeated Holdout KNN
 library(rminer)
 full_accuracy = 0
@@ -480,9 +614,9 @@ list_accuracy <- list()
 
 for(i in 1:100) {
   H = holdout(bank$y, ratio = 2/3, mode="random", seed = NULL)
-  knnmodel <- knn3(y~., data=bank_drop[H$tr,])
-  knnpredict <- predict(knnmodel, bank_drop[H$ts,1:12], type="class")
-  result<- confusionMatrix(knnpredict, bank_drop[H$ts,]$y)
+  knnmodel <- knn3(y~., data=bank[H$tr,], k = 177)
+  knnpredict <- predict(knnmodel, bank[H$ts,1:16], type="class")
+  result<- confusionMatrix(knnpredict, bank[H$ts,]$y)
   accuracy <- result$overall['Accuracy']
   cat("batch: ",i,
       "accuracy: ",accuracy,"\n")
@@ -490,14 +624,41 @@ for(i in 1:100) {
   list_accuracy[[i]] <- accuracy
 }
 
-### Plot
+k =### Plot
 cat("Tree: ",
     "Accuracy: ", full_accuracy/100, "\n")
 x <- c(1:100)
 plot(x, list_accuracy, type="o")
        
-######################################## Repeated holdout buat ANN ########################################
+########################################## Cross Validation kNN k = 177 ####################################
+#ingat ubah data bank menjadi NUMERIK kembali untuk menjalankan ini
+### Membuat 10 fold
+folds <- cut(seq(1,nrow(bank)), breaks=10, label=FALSE)
+full_accuracy_cv = 0
+list_accuracy_cv <- list()
 
+for(i in 1:10) {
+  testIndex <- which(folds == i, arr.ind = TRUE)
+  testData <- bank[testIndex, ]
+  trainData <- bank[-testIndex, ]
+  knnModel212 <- knn3(y~., data = trainData, k = 177)
+  knnPredict212 <- predict(knnModel212, testData[,1:16], type="class")
+  result <- confusionMatrix(knnPredict212, testData$y)
+  accuracy <- result$overall['Accuracy']
+  
+  cat("batch: ",i,
+      "accuracy: ",accuracy,"\n")
+  full_accuracy_cv = full_accuracy_cv + accuracy
+  list_accuracy_cv[[i]] <- accuracy
+}
+
+### Plot
+cat("Tree: ",
+    "Accuracy: ", full_accuracy_cv/10,"\n")
+x <- c(1:10)
+plot(x, list_accuracy_cv, type="o")
+########################################## Repeated holdout buat ANN ########################################
+#ingat ubah data bank menjadi NUMERIK kembali untuk menjalankan ini
 # Repeated Holdout ANN
 library(rminer)
 full_accuracy = 0
@@ -505,10 +666,11 @@ list_accuracy <- list()
 
 for(i in 1:100) {
   H = holdout(bank$y, ratio = 2/3, mode="random", seed = NULL)
-  nn1 <- nnet(y~., data=bank_drop[H$tr,])
-  predict_nn1 <- predict(nn1, bank_drop[H$ts,1:12], size = 10, maxit = 300,
-                         trace = FALSE)
-  result<- confusionMatrix(predict_nn1, bank_drop[H$ts,]$y)
+  nn1 <- nnet(y~., data = bank[H$tr,], size = 10, maxit = 300,
+              trace = FALSE)
+  predict_nn1 <- predict(nn1, newdata = bank[H$ts,1:16], type = 'class')
+  pred1 <- factor(predict_nn1)
+  result<- confusionMatrix(pred1, bank[H$ts,]$y)
   accuracy <- result$overall['Accuracy']
   cat("batch: ",i,
       "accuracy: ",accuracy,"\n")
@@ -517,10 +679,39 @@ for(i in 1:100) {
 }
 
 
-
 ### Plot
 cat("Tree: ",
     "Accuracy: ", full_accuracy/100, "\n")
 x <- c(1:100)
 plot(x, list_accuracy, type="o")
 
+
+########################################## Cross Validation ANN ####################################
+#ingat ubah data bank menjadi NUMERIK kembali untuk menjalankan ini
+### Membuat 10 fold
+folds <- cut(seq(1,nrow(bank)), breaks=10, label=FALSE)
+full_accuracy_cv = 0
+list_accuracy_cv <- list()
+
+for(i in 1:10) {
+  testIndex <- which(folds == i, arr.ind = TRUE)
+  testData <- bank[testIndex, ]
+  trainData <- bank[-testIndex, ]
+  nn1 <- nnet(y~., data = trainData, size = 10, maxit = 300,
+              trace = FALSE)
+  predict_nn1 <- predict(nn1, newdata = testData[,1:16], type = 'class')
+  pred1 <- factor(predict_nn1)
+  result <- confusionMatrix(pred1, testData$y)
+  accuracy <- result$overall['Accuracy']
+  
+  cat("batch: ",i,
+      "accuracy: ",accuracy,"\n")
+  full_accuracy_cv = full_accuracy_cv + accuracy
+  list_accuracy_cv[[i]] <- accuracy
+}
+
+### Plot
+cat("Tree: ",
+    "Accuracy: ", full_accuracy_cv/10,"\n")
+x <- c(1:10)
+plot(x, list_accuracy_cv, type="o")
